581eb9fc45c653b2987a79a4e00586d39012f5c2
Tamás Kecskeméti <tamas.kecskemeti@stud.uni-corvinus.hu>
2021-10-02 12:16:32 +0200

    Create, format and load data from Data Source (into Excel)

--- InputOutput/InputOutput/Form1.cs
+++ InputOutput/InputOutput/Form1.cs
@@ -7,14 +7,174 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace InputOutput
 {
     public partial class Form1 : Form
     {
+        List<Flat> flats;
+        RealEstateEntities context = new RealEstateEntities();
+        string[] headers;
+
+        Excel.Application xlApp; //A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; //A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; //Munkalap a munkafüzeten belül
         public Form1()
         {
             InitializeComponent();
+
+            LoadData();
+            CreateExcel();
         }
+
+        void LoadData()
+        {
+            flats = context.Flats.ToList();
+        }
+
+        void CreateExcel()
+        {
+            try
+            {
+                //Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                //Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                //Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                //Tábla létrehozása
+                CreateTable();
+
+                //Tábla formázása
+                FormatTable();
+
+                //Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch(Exception ex) //Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                //Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        void CreateTable()
+        {
+            headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+            }
+
+            object[,] values = new object[flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (var x in flats)
+            {
+                values[counter, 0] = x.Code;
+                values[counter, 1] = x.Vendor;
+                values[counter, 2] = x.Side;
+                values[counter, 3] = x.District;
+                if (x.Elevator) values[counter, 4] = "Van"; else values[counter, 4] = "Nincs";
+                values[counter, 5] = x.NumberOfRooms;
+                values[counter, 6] = x.FloorArea;
+                values[counter, 7] = x.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+
+            xlSheet.get_Range(
+                GetCell(2, 1),
+                GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            int[] rangevalues = new int[values.Length];
+            
+            for (int i = 1; i <= values.Length; i++)
+            {
+                rangevalues[i-1] = i;
+            }
+
+            xlSheet.get_Range(
+                GetCell(2, 9),
+                GetCell(1 + values.GetLength(0), 9)).Value2 = "=SUM(G:G)";
+
+        }
+
+        private void FormatTable()
+        {
+            //Fejléc formázása
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            //Táblázat formázása
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+
+            xlSheet.get_Range(
+                GetCell(2,1),
+                GetCell(lastRowID, headers.Length)).BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range firstColumnRange = xlSheet.get_Range(
+                GetCell(2, 1),
+                GetCell(lastRowID, 1));
+
+            firstColumnRange.Font.Bold = true;
+            firstColumnRange.Interior.Color = Color.LightYellow;
+
+            Excel.Range lastColumnRange = xlSheet.get_Range(
+                GetCell(2, 9),
+                GetCell(lastRowID, 9));
+
+            lastColumnRange.Interior.Color = Color.LightGreen;
+            lastColumnRange.NumberFormat = "0.00";
+
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
     }
 }
