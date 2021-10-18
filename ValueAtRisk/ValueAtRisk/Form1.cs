5d377ccf359048bb78e726734372f46a4cc2c503
Tamás Kecskeméti <tamas.kecskemeti@stud.uni-corvinus.hu>
2021-10-18 14:39:11 +0200

    Portfolio into the memory

--- ValueAtRisk/ValueAtRisk/Form1.cs
+++ ValueAtRisk/ValueAtRisk/Form1.cs
@@ -7,18 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ValueAtRisk.Entities;
 
 namespace ValueAtRisk
 {
     public partial class Form1 : Form
     {
         List<Tick> ticks;
+        List<PortfolioItem> Portfolio;
         PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
             ticks = context.Ticks.ToList();
             dataGridView1.DataSource = ticks;
+            CreatePortfolio();
         }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+
     }
 }
