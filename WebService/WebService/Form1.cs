590f42fe1d963da0c3149965f483b6c62c50d464
Tamás Kecskeméti <tamas.kecskemeti@stud.uni-corvinus.hu>
2021-10-26 17:04:00 +0200

    Dynamic functions to chart

--- WebService/WebService/Form1.cs
+++ WebService/WebService/Form1.cs
@@ -20,12 +20,16 @@ namespace WebService
         public Form1()
         {
             InitializeComponent();
+            RefreshData();
+        }
+
+        void RefreshData()
+        {
+            Rates.Clear();
             dataGridView1.DataSource = Rates;
             chartRateData.DataSource = Rates;
             ProcessXML(CallWebService());
             ShowData();
-
-            Console.WriteLine(CallWebService());
         }
 
         private string CallWebService()
@@ -33,9 +37,9 @@ namespace WebService
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
@@ -83,6 +87,21 @@ namespace WebService
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
