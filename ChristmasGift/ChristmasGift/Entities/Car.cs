aa059830a5746f1e82a765d1c60f3eb57965eabf
Tamás Kecskeméti <tamas.kecskemeti@stud.uni-corvinus.hu>
2021-11-07 20:38:27 +0100

    Create Car class

---
+++ ChristmasGift/ChristmasGift/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using ChristmasGift.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasGift.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics input)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            input.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
