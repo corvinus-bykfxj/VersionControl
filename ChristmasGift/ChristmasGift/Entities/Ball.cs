539489030e3c9c15b04f998fb7506a152f1b61ed
Tamás Kecskeméti <tamas.kecskemeti@stud.uni-corvinus.hu>
2021-11-07 19:47:08 +0100

    Create Ball class

---
+++ ChristmasGift/ChristmasGift/Entities/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ChristmasGift.Entities
+{
+    class Ball : Label
+    {
+        public Ball()
+        {
+            this.AutoSize = false;
+            this.Width = 50;
+            this.Height = this.Width;
+            this.Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics input)
+        {
+            input.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            this.Left += 1;
+        }
+    }
+}
