d2feff0bf53daee19aed0573a43e6e5c7840177f
Tamás Kecskeméti <tamas.kecskemeti@stud.uni-corvinus.hu>
2021-11-28 13:51:58 +0100

    Create Password Regex function

--- UnitTestExample/UnitTestExample/Controllers/AccountController.cs
+++ UnitTestExample/UnitTestExample/Controllers/AccountController.cs
@@ -27,7 +27,7 @@ namespace UnitTestExample.Controllers
                     "A megadott e-mail cím nem megfelelő!");
             if(!ValidateEmail(email))
                 throw new ValidationException(
-                    "A megadottt jelszó nem megfelelő!\n" +
+                    "A megadott jelszó nem megfelelő!\n" +
                     "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
 
             var account = new Account()
@@ -50,7 +50,26 @@ namespace UnitTestExample.Controllers
 
         public bool ValidatePassword(string password)
         {
-            return true;
+            if (Regex.IsMatch(password, @"[a-z]{1,}") == false)
+            {
+                return false;
+            }
+            else if((Regex.IsMatch(password, @"[A-Z]{1,}") == false))
+            {
+                return false;
+            }
+            else if((Regex.IsMatch(password, @"[0-9]{1,}") == false))
+            {
+                return false;
+            }
+            else if((Regex.IsMatch(password, @"^[a-zA-Z0-9]{8,}$") == false))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
