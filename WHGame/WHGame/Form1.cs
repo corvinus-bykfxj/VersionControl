bd219e61ea66b3da8d111bb2783dc90aa1ed6b81
Tamás Kecskeméti <tamas.kecskemeti@stud.uni-corvinus.hu>
2021-12-12 10:45:06 +0100

    New generation

--- WHGame/WHGame/Form1.cs
+++ WHGame/WHGame/Form1.cs
@@ -31,7 +31,11 @@ namespace WHGame
             //gc.Start(true);
 
             gc.GameOver += Gc_GameOver;
-            gc.AddPlayer(nbrOfSteps);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
             gc.Start();
         }
 
@@ -44,6 +48,22 @@ namespace WHGame
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
